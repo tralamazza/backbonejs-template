e803f15a1c9c488c159be4c9e70ce168472fa128
Daniel Tralamazza <daniel@tralamazza.com>
2012-01-11 14:54:10 -0200

    meta viewport

--- js/libs/jquery.mobile/require-jquery.mobile.js
+++ js/libs/jquery.mobile/require-jquery.mobile.js
@@ -3,9 +3,9 @@ define([
   'order!libs/jquery.mobile/jquery.mobile-1.0.min'
 ], function($) {
   $(document).ready(function() {
-	$("head").append($("<meta name='viewport' content='width=device-width, initial-scale=1'>"));
+    $("head").append($("<meta name='viewport' content='width=device-width, initial-scale=1'>"));
 
-	if (document.createStyleSheet) {
+    if (document.createStyleSheet) {
       document.createStyleSheet('js/libs/jquery.mobile/jquery.mobile-1.0.min.css');
     } else {
       $("head").append($("<link rel='stylesheet' href='js/libs/jquery.mobile/jquery.mobile-1.0.min.css' />"));
