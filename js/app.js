07cf79f3cba1f424de8b5b6f0458ef24d155ead3
Daniel Tralamazza <daniel@tralamazza.com>
2012-01-11 14:55:05 -0200

    jqm hash routing

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ define([
   'routers/main',
 ], function($, _, Backbone, Router){
   var initialize = function(){
-	// Disables JQM routing (*) avoiding backbone conflict
-	// Transitions must manually call $.mobile.changePage (**)
-	// * http://jquerymobile.com/test/docs/api/globalconfig.html
-	// ** http://jquerymobile.com/demos/1.0/docs/pages/page-navmodel.html
-	$.mobile.hashListeningEnabled = false;
+    // Disables JQM routing (*) avoiding backbone conflict
+    // Transitions must manually call $.mobile.changePage (**)
+    // * http://jquerymobile.com/test/docs/api/globalconfig.html
+    // ** http://jquerymobile.com/demos/1.0/docs/pages/page-navmodel.html
+    $.mobile.hashListeningEnabled = false;
 
     Router.initialize();
   }
